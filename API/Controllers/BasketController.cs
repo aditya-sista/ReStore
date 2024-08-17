b328186d5aa19607079746432cc58885a3183d8e
Aditya Sista <aditya16791@gmail.com>
2024-08-17 22:29:08 +0530

    End of section 7

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -33,7 +33,7 @@ public class BasketController : BaseApiController
 
         var product = await _context.Products.FindAsync(productId);
 
-        if (product is null) return NotFound();
+        if (product is null) return BadRequest(new ProblemDetails{ Title = "Product not found" });
         
         basket.AddItem(product, quantity);
 
